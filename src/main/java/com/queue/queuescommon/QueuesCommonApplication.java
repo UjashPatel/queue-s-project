43641f7a370076806815f0ae56e9ef034bf9151c
Akash <akash.patel@bonoboz.in>
2020-11-08 19:58:07 +0530

    First commit

---
+++ src/main/java/com/queue/queuescommon/QueuesCommonApplication.java
@@ -0,0 +1,13 @@
+package com.queue.queuescommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuesCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuesCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/queue/queuescommon/QueuesCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queue.queuescommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuesCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
